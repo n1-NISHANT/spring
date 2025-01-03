81b819ad54b24376f89464cdf90931bcf9762b5d
n1-NISHANT <nk98nishant@gmail.com>
2025-01-03 21:42:43 +0530

    Update A.java
    This is normal java class whish have two fields those fields are dependent. we will assign those fields with the help of setter function.

--- Basic/A.java
+++ Basic/A.java
@@ -1,1 +1,21 @@
+public class A{
+      int a;
+      String msg;
 
+  statc{
+        System.out.println("sctatic blck in A class");
+  }
+  public A(){
+         System.out.println("default cunstructor in A");
+  }
+  public void setA(int a){
+          this.a = a;
+  }
+  public void setMsg(String msg){
+          this.msg = msg;
+  }
+
+  public String toString(){
+      return "a : "+a+" msg : "+msg;
+  }
+}
