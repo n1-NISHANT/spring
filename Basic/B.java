3493eefe071f887c3a9465aeb29c022695b86ea4
n1-NISHANT <nk98nishant@gmail.com>
2025-01-03 21:51:36 +0530

    Update B.java
    This is also normal java class but, in this class, we assign fields value with the help of constructor, and we also override  toString method to get information about the class.

--- Basic/B.java
+++ Basic/B.java
@@ -1,1 +1,17 @@
+public class B{
+     int b;
+     String str;
 
+  static{
+      System.out.println("static bloc in B");
+  }
+
+  public B(int b, String str){
+      this.b = b;
+      this.str = str;
+  }
+
+  public String toString(){
+          return "b : "+b + " str : "+str;
+  }
+}
