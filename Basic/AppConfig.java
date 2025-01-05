c20d46ea72a25e40fa8419d97fca913ec1e47624
n1-NISHANT <nk98nishant@gmail.com>
2025-01-05 23:01:20 +0530

    Update AppConfig.java

--- Basic/AppConfig.java
+++ Basic/AppConfig.java
@@ -2,8 +2,19 @@
 @Configuration
 public class AppConfig{
 
+  @Bean
   public A createA{
     A a = new A();
     return a;
+  }
+  @Bean(name = "b")
+  public B createB{
+    B b = new B();
+    return b;
+  }
+   @Bean("hello")
+  public Hello createHello{
+    Hello hello = new Hello();
+    return hello;
   }
 }
