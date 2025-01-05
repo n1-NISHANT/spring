ef8f098dfbd116013a5fc51622bca55d9cf16d20
n1-NISHANT <nk98nishant@gmail.com>
2025-01-05 23:08:05 +0530

    Update TestMain.java

--- Basic/TestMain.java
+++ Basic/TestMain.java
@@ -1,6 +1,9 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestMain{
   public static void main (String [] args){
-      
+      ApplicationContext cgf = new AnnotationConfigApplicationContext(AppConfig.java);
+    
   } 
 }
