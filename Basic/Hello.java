139460c0387fd327518b7ea9090fb8be6b5c6dec
n1-NISHANT <nk98nishant@gmail.com>
2025-02-21 22:33:10 +0530

    Update Hello.java

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -2,12 +2,10 @@ public class Hello{
 
     A aobj;
     B bobj;
-
     public void setAobj(A aobj) {
 		System.out.println("setter method in Hello");
 		this.aobj = aobj;
 	}
-	
 	public Hello(B bobj) {
 		System.out.println("1 param constructor Hello Class");
 		this.bobj = bobj;
